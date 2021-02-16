c358a7b34714b9d619f636b36039167950fa1ea6
velmurgan pillai <karthikpillai1323@gmail.com>
2021-02-17 00:13:26 +0530

    changed the file name

--- Car.py
+++ Car.py
@@ -2,6 +2,12 @@ import heapq
 from collections import defaultdict, OrderedDict
 
 class Car:
-    def __init__(self, car_register_no, car_color):
-        self.car_register_no = car_register_no
-        self.car_color = car_color
+    def __init__(self, registration_number, color):
+        self.registration_number = registration_number
+        self.color = color
+
+    def showCarDetails(self):
+
+        car_details = "Car [registration_number=" + self.registration_number + ", color=" + self.color + "]"
+
+        return car_details
